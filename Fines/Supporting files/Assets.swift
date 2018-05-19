5c7e8bdc9bc2a5117b135ec022837557a9a3debb
Владислав Пуличев <vlad.pulichev777@gmail.com>
2018-05-19 23:41:25 +0300

    Update assets enum

---
+++ Fines/Supporting files/Assets.swift
@@ -0,0 +1,26 @@
+//
+//  Assets.swift
+//  Fines
+//
+//  Created by Владислав Пуличев on 19.05.2018.
+//  Copyright © 2018 Владислав Пуличев. All rights reserved.
+//
+
+enum Assets: String {
+  
+  case onboardingVehiclePlate        = "onboarding_vehicle_plate"
+  case onboardingVehicleRegistration = "onboarding_vehicle_registration"
+  case onboardingDriverRegistration  = "onboarding_driver_registration"
+  
+  case capabilitiesPushPhone         = "capabilities_push_phone"
+  case capabilitiesPushPad           = "capabilities_push_pad"
+  case capabilitiesPayPhone          = "capabilities_pay_phone"
+  case capabilitiesPayPad            = "capabilities_pay_pad"
+  case capabilitiesDiscountPhone     = "capabilities_discount_phone"
+  case capabilitiesDiscountPad       = "capabilities_discount_pad"
+  case capabilitiesBill              = "capabilities_bill"
+  case capabilitiesRedeemed          = "capabilities_redeemed"
+  
+  case commonCheckPhone              = "common_check_phone"
+  case commonCheckPad                = "common_check_pad"
+}
